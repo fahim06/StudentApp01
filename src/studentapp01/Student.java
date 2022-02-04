b1869257fe640e9607d4200fb0aa727713045af1
fahim06 <fahim.yusuf06@gmail.com>
2022-02-04 21:56:34 +0600

    first commit

---
+++ src/studentapp01/Student.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentapp01;
+
+/**
+ *
+ * @author Sakib-Pc
+ */
+public class Student {
+    String studetname, studentgrade;
+    double studentcgpa;
+    public Student(String studentname, String studentgrade, double studentcgpa)
+    {
+        this.studetname = studentname;
+        this.studentgrade = studentgrade;
+        this.studentcgpa = studentcgpa;
+    }
+    
+}
---
+++ src/studentapp01/StudentApp01.java
@@ -0,0 +1,13 @@
+
+package studentapp01;
+
+
+public class StudentApp01 {
+
+    
+    public static void main(String[] args) {
+        WelcomeJFrame welcomeUi = new WelcomeJFrame();
+        welcomeUi.show();
+    }
+    
+}
